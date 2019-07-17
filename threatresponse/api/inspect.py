24c1c6bdcd83553c2ad9c752ede3770013f05736
Gevorg Davoian <gdavoian@gmail.com>
2019-07-17 10:31:24 +0300

    Implement InspectAPI

--- threatresponse/api/inspect.py
+++ threatresponse/api/inspect.py
@@ -1,5 +1,16 @@
 from .base import BaseAPI
+from ..utils import raise_for_status
 
 
 class InspectAPI(BaseAPI):
-    pass
+
+    def inspect(self, payload):
+        """
+        https://visibility.amp.cisco.com/iroh/iroh-inspect/index.html#!/INSPECT/post_iroh_iroh_inspect_inspect
+        :param payload: JSON
+        :return: JSON
+        """
+        url = self.build_full_url('/iroh/iroh-inspect/inspect')
+        response = self._request.post(url, json=payload)
+        raise_for_status(response)
+        return response.json()
