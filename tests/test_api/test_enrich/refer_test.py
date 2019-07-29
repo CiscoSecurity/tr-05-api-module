90c7b09f8148c106569d93a0edeba1520af1c4a0
skhare <skhare@softserveinc.com>
2019-07-29 17:44:31 +0300

    removed MagicMock and changed patch

--- tests/test_api/test_enrich/refer_test.py
+++ tests/test_api/test_enrich/refer_test.py
@@ -1,38 +1,36 @@
 from unittest import TestCase
-from mock import patch, MagicMock
 
+from tests.common import patch
+from threatresponse.request.base import Request
 from threatresponse.api.enrich.refer import ReferAPI
 
 
 class ReferTestCase(TestCase):
 
-    @patch('threatresponse.request.base.Request')
+    @patch(Request)
     def test_observables(self, request):
         payload = [
             {"value": "string",
              "type": "file_path"}
         ]
-        request.post.side_effect = lambda *args, **kwargs: MagicMock()
 
         api = ReferAPI(request)
         api.observables(payload)
 
         request.post.assert_called_once_with('/iroh/iroh-enrich/refer/observables', json=payload)
 
-    @patch('threatresponse.request.base.Request')
+    @patch(Request)
     def test_sighting(self, request):
         payload = {'hello': 'world'}
-        request.post.side_effect = lambda *args, **kwargs: MagicMock()
 
         api = ReferAPI(request)
         api.sighting(payload)
 
         request.post.assert_called_once_with('/iroh/iroh-enrich/refer/sighting', json=payload)
 
-    @patch('threatresponse.request.base.Request')
+    @patch(Request)
     def test_sighting_ref(self, request):
         payload = 'hello world'
-        request.post.side_effect = lambda *args, **kwargs: MagicMock()
 
         api = ReferAPI(request)
         api.sighting_ref(payload)
