283e5c57c99aec2173ce256085f1918f0a5a1aaa
Gevorg Davoian <gdavoian@gmail.com>
2019-08-22 16:21:27 +0300

    Support several API regions

--- threatresponse/client.py
+++ threatresponse/client.py
@@ -1,21 +1,43 @@
 from .api.enrich import EnrichAPI
 from .api.inspect import InspectAPI
+from .exceptions import UnsupportedRegionError
 from .request.authorized import AuthorizedRequest
 from .request.logged import LoggedRequest
-from .request.standard import StandardRequest
 from .request.relative import RelativeRequest
+from .request.standard import StandardRequest
 
 
 class ThreatResponse(object):
+    SUPPORTED_REGIONS = ('', '.eu', '.apjc')
+
+    VISIBILITY_URL_PATTERN = 'https://visibility{region}.amp.cisco.com/'
 
     def __init__(self, client_id, client_password, **options):
         request = StandardRequest()
 
-        if options.get('logger'):
+        logger = options.get('logger')
+        if logger is not None:
             request = LoggedRequest(request, options['logger'])
 
         request = AuthorizedRequest(request, client_id, client_password)
-        request = RelativeRequest(request, 'https://visibility.amp.cisco.com/')
+
+        region = options.get('region') or ''
+        if len(region) > 0 and not region.startswith('.'):
+            region = '.' + region
+
+        if region not in self.SUPPORTED_REGIONS:
+            # Use `repr` to make each region enclosed in quotes.
+            raise UnsupportedRegionError(
+                'Region {} is unsupported, must be one of: {}.'.format(
+                    repr(region),
+                    ', '.join(map(repr, self.SUPPORTED_REGIONS)),
+                )
+            )
+
+        request = RelativeRequest(
+            request,
+            self.VISIBILITY_URL_PATTERN.format(region=region),
+        )
 
         self._inspect = InspectAPI(request)
         self._enrich = EnrichAPI(request)
