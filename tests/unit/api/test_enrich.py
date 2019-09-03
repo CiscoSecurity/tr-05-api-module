a5fa7845832d8a8a936df697dc4d20f2b288cc7b
Konstantin Ogulchansky <spaceiseternal@gmail.com>
2019-09-03 16:15:24 +0300

    Generalized `test_enrich` even more.

--- tests/unit/api/test_enrich.py
+++ tests/unit/api/test_enrich.py
@@ -5,86 +5,82 @@ from threatresponse.api import EnrichAPI
 
 
 def test_health_fails():
-    class TestError(Exception):
-        pass
-
-    response = MagicMock()
-    response.raise_for_status.side_effect = TestError('Oops!')
-
-    request = MagicMock()
-    request.post.return_value = response
-
-    api = EnrichAPI(request)
-    with pytest.raises(TestError):
-        api.health()
-
-    request.post.assert_called_once_with('/iroh/iroh-enrich/health')
-
-    response.raise_for_status.assert_called_once_with()
+    assert_fails(
+        lambda api: api.health(),
+        url='/iroh/iroh-enrich/health'
+    )
 
 
 def test_deliberate_observables_succeeds():
     assert_succeeds(
         lambda api, payload: api.deliberate.observables(payload),
-        '/iroh/iroh-enrich/deliberate/observables'
+        payload=[{'ham': 'egg'}],
+        url='/iroh/iroh-enrich/deliberate/observables'
     )
 
 
 def test_deliberate_observables_fails():
     assert_fails(
         lambda api, payload: api.deliberate.observables(payload),
-        '/iroh/iroh-enrich/deliberate/observables'
+        payload=[{'ham': 'egg'}],
+        url='/iroh/iroh-enrich/deliberate/observables'
     )
 
 
 def test_observe_observables_succeeds():
     assert_succeeds(
         lambda api, payload: api.observe.observables(payload),
-        '/iroh/iroh-enrich/observe/observables'
+        payload=[{'ham': 'egg'}],
+        url='/iroh/iroh-enrich/observe/observables'
     )
 
 
 def test_observe_observables_fails():
     assert_fails(
         lambda api, payload: api.observe.observables(payload),
-        '/iroh/iroh-enrich/observe/observables'
+        payload=[{'ham': 'egg'}],
+        url='/iroh/iroh-enrich/observe/observables'
     )
 
 
 def test_refer_observables_succeeds():
     assert_succeeds(
         lambda api, payload: api.refer.observables(payload),
-        '/iroh/iroh-enrich/refer/observables'
+        payload=[{'ham': 'egg'}],
+        url='/iroh/iroh-enrich/refer/observables'
     )
 
 
 def test_refer_observables_fails():
     assert_fails(
         lambda api, payload: api.refer.observables(payload),
-        '/iroh/iroh-enrich/refer/observables'
+        payload=[{'ham': 'egg'}],
+        url='/iroh/iroh-enrich/refer/observables'
     )
 
 
-def assert_succeeds(invoke, url):
+def assert_succeeds(invoke, url, payload=None):
     response = MagicMock()
 
     request = MagicMock()
     request.post.return_value = response
 
-    payload = [{'foo': 'bar'}]
-
     api = EnrichAPI(request)
-    invoke(api, payload)
 
-    request.post.assert_called_once_with(
-        url,
-        json=payload,
-    )
+    if payload is not None:
+        invoke(api, payload)
+    else:
+        invoke(api)
+
+    if payload is not None:
+        request.post.assert_called_once_with(url, json=payload)
+    else:
+        request.post.assert_called_once_with(url)
 
     response.json.assert_called_once_with()
 
 
-def assert_fails(invoke, url):
+def assert_fails(invoke, url, payload=None):
     class TestError(Exception):
         pass
 
@@ -94,16 +90,16 @@ def assert_fails(invoke, url):
     request = MagicMock()
     request.post.return_value = response
 
-    payload = [{'foo': 'bar'}]
-
     api = EnrichAPI(request)
     with pytest.raises(TestError):
-        invoke(api, payload)
+        if payload is not None:
+            invoke(api, payload)
+        else:
+            invoke(api)
 
-    request.post.assert_called_once_with(
-        url,
-        json=payload,
-    )
+    if payload is not None:
+        request.post.assert_called_once_with(url, json=payload)
+    else:
+        request.post.assert_called_once_with(url)
 
     response.raise_for_status.assert_called_once_with()
-
