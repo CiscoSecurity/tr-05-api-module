834e5cd0a6a008f9f4827cbc5252ed45a85a59d6
ablahun <ablahun@cisco.com>
2019-09-06 15:39:23 +0300

    CCTRI-171: Add 'Attack Pattern' endpoints.

--- threatresponse/api/intel.py
+++ threatresponse/api/intel.py
@@ -5,6 +5,8 @@ from .base import API
 class IntelAPI(API):
     __router, route = Router.new()
 
+    # Actor
+
     @route('actor.post')
     def _perform(self, payload):
         """
@@ -77,6 +79,85 @@ class IntelAPI(API):
 
         response = self._request.get(
             '/ctia/actor/search',
+            params=params
+        )
+        response.raise_for_status()
+        return response.json()
+
+    # Attack Pattern
+
+    @route('attack_pattern.post')
+    def _perform(self, payload):
+        """
+        https://private.intel.amp.cisco.com/index.html#!/Attack_Pattern/post_ctia_attack_pattern
+        """
+
+        response = self._request.post(
+            '/ctia/attack-pattern',
+            json=payload
+        )
+        response.raise_for_status()
+        return response.json()
+
+    @route('attack_pattern.delete')
+    def _perform(self, id_):
+        """
+        https://private.intel.amp.cisco.com/index.html#!/Attack_Pattern/delete_ctia_attack_pattern_id
+        """
+
+        response = self._request.delete(
+            '/ctia/attack-pattern/{}'.format(id_)
+        )
+        response.raise_for_status()
+        return response.json()
+
+    @route('attack_pattern.get')
+    def _perform(self, id_, **params):
+        """
+        https://private.intel.amp.cisco.com/index.html#!/Attack_Pattern/get_ctia_attack_pattern_id
+        """
+
+        response = self._request.get(
+            '/ctia/attack-pattern/{}'.format(id_),
+            params=params
+        )
+        response.raise_for_status()
+        return response.json()
+
+    @route('attack_pattern.put')
+    def _perform(self, id_, payload):
+        """
+        https://private.intel.amp.cisco.com/index.html#!/Attack_Pattern/put_ctia_attack_pattern_id
+        """
+
+        response = self._request.put(
+            '/ctia/attack-pattern/{}'.format(id_),
+            json=payload
+        )
+        response.raise_for_status()
+        return response.json()
+
+    @route('attack_pattern.external_id')
+    def _perform(self, id_, **params):
+        """
+        https://private.intel.amp.cisco.com/index.html#!/Attack_Pattern/get_ctia_attack_pattern_external_id_external_id
+        """
+
+        response = self._request.get(
+            '/ctia/attack-pattern/external_id/{}'.format(id_),
+            params=params
+        )
+        response.raise_for_status()
+        return response.json()
+
+    @route('attack_pattern.search')
+    def _perform(self, **params):
+        """
+        https://private.intel.amp.cisco.com/index.html#!/Attack_Pattern/get_ctia_attack_pattern_search
+        """
+
+        response = self._request.get(
+            '/ctia/attack-pattern/search',
             params=params
         )
         response.raise_for_status()
