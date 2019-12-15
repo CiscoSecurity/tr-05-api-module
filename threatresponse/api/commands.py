ccbe9a58d0826dac93b0d288e8d327a096e3562b
Nikita Gurtovoy <cawabanga2811@gmail.com>
2019-12-15 14:46:29 +0200

    Initial implementation of targets (from Gosha) (#53)
    * Initial implementation of targets (from Gosha)
    
    * Fixing pep-8

--- threatresponse/api/commands.py
+++ threatresponse/api/commands.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from .base import API
 from .routing import Router
 
@@ -8,7 +10,8 @@ class CommandsAPI(API):
     @route('verdict')
     def _perform(self, payload, **kwargs):
         """
-        Command allow to simple query CTR for a verdict for a bunch of observables
+        Command allow to simple query CTR
+        for a verdict for a bunch of observables
         """
 
         response = self._post(
@@ -26,7 +29,9 @@ class CommandsAPI(API):
         for module in response.get('data', []):
             module_name = module['module']
 
-            for doc in module.get('data', {}).get('verdicts', {}).get('docs', []):
+            for doc in module.get('data', {})\
+                    .get('verdicts', {})\
+                    .get('docs', []):
                 verdicts.append({
                     'observable_value': doc['observable']['value'],
                     'observable_type': doc['observable']['type'],
@@ -35,3 +40,45 @@ class CommandsAPI(API):
                     'disposition_name': doc['disposition_name']
                 })
         return {"response": response, "verdicts": verdicts}
+
+    @route('targets')
+    def _perform(self, payload, **kwargs):
+        """
+        Command allow to simple query CTR for a targets
+        for a bunch of observables
+        """
+
+        response = self._post(
+            '/iroh/iroh-inspect/inspect',
+            json={'content': str(payload)},
+            **kwargs
+        )
+
+        response = self._post(
+            '/iroh/iroh-enrich/observe/observables',
+            json=response,
+            **kwargs
+        )
+
+        result = []
+
+        for module in response.get('data', []):
+            module_name = module['module']
+            targets = []
+
+            for doc in module.get('data', {}) \
+                    .get('sightings', {}) \
+                    .get('docs', []):
+
+                for target in doc.get('targets', []):
+                    element = deepcopy(target)
+                    element.pop('observed_time', None)
+                    if element not in targets:
+                        targets.append(element)
+
+            result.append({
+                'module': module_name,
+                'targets': targets
+            })
+
+        return {"response": response, "targets": result}
