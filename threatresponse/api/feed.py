db55944f146765a98655afb78e3d75fbc8495e8c
GeorgiyII <gosha1104@gmail.com>
2020-03-27 15:20:53 +0200

    added feed endpoint

---
+++ threatresponse/api/feed.py
@@ -0,0 +1,28 @@
+from .entity import EntityAPI
+from .routing import Router
+from .. import urls
+
+
+class FeedAPI(EntityAPI):
+    """https://private.intel.amp.cisco.com/index.html#/Feed"""
+
+    __router, route = Router.new()
+
+    def __init__(self, request):
+        super(FeedAPI, self).__init__(request, '/ctia/feed')
+
+    @route('view')
+    def _perform(self, id_, share_token, **kwargs):
+        return self._get(
+            urls.join(self._url, id_, 'view'),
+            params={'s': share_token},
+            **kwargs
+        )
+
+    @route('view.txt')
+    def _perform(self, id_, share_token, **kwargs):
+        return self._get(
+            urls.join(self._url, id_, 'view.txt'),
+            params={'s': share_token},
+            **kwargs
+        )
