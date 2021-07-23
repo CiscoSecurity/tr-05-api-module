53494ec52b7288666df735ee55abc1f7478ccfdd
YuriiTsekhovyi <ytsek@softserveinc.com>
2021-07-23 15:22:34 +0300

    remove outdated package enterpoint

--- code/__init__.py
+++
@@ -1,5 +0,0 @@
-# Make the main class importable from the root package directly.
-from code.api.threatresponse.client import ThreatResponse
-
-# Load the current version meta-attribute into the package.
-from code.api.threatresponse.version import __version__
