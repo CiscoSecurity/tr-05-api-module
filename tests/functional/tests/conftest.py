9b9c68434baa0c7e8d8d2c0b3c570d4b2f2e2646
Olena Shynkarenko <oshynk@softserveinc.com>
2021-08-09 12:53:45 +0300

    Fix ThreatResponse import

--- tests/functional/tests/conftest.py
+++ tests/functional/tests/conftest.py
@@ -5,7 +5,7 @@ from ctrlibrary.core.datafactory import gen_string
 from ctrlibrary.threatresponse import token
 from ctrlibrary.core import settings
 from ctrlibrary.threatresponse.profile import update_org
-from threatresponse.client import ThreatResponse
+from threatresponse import ThreatResponse
 
 
 def pytest_collection_modifyitems():
